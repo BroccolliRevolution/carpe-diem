5f81d2886b87b99662b164f90144c5aa7f6caaf1
peter.kapusta <peter.kapusta@faceup.com>
2023-02-20 16:48:43 +0100

    chore: db connections

---
+++ src/components/activities/edit-activities.tsx
@@ -0,0 +1,59 @@
+import { Button, Grid, TextField } from "@mui/material"
+import { useRef, useState } from "react"
+import UseActivities from "./useActivities"
+
+const EditActivities = () => {
+  const { activities, setActivities } = UseActivities()
+  const [title, setTitle] = useState<string>("")
+  const titleText = useRef(null)
+
+  const saveActivity = () => {
+    const activity = { title, done: false }
+    setActivities([...activities, activity])
+    setTitle("")
+  }
+
+  return (
+    <Grid container spacing={3} height={"100%"}>
+      <Grid item xs={12} md={7} lg={7}>
+        <h3>Activities</h3>
+        <Grid container>
+          <Grid item xs={7}>
+            <TextField
+              ref={titleText}
+              fullWidth
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+              onKeyDown={(ev) => {
+                if (ev.key === "Enter") {
+                  ev.preventDefault()
+                  saveActivity()
+                }
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={2} alignItems="stretch" style={{ display: "flex" }}>
+            <Button onClick={saveActivity} variant="contained">
+              +
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} alignItems="stretch" style={{ display: "flex" }}>
+          <ul style={{ listStyleType: "none", padding: 0 }}>
+            {activities.map((activity) => (
+              <li key={activity.title}>{activity.title}</li>
+            ))}
+          </ul>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={5} lg={5}>
+        <h3>Daily Log</h3>
+      </Grid>
+    </Grid>
+  )
+}
+export default EditActivities
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,30 +8,6 @@ import * as React from "react"
 
 import Header from "./header"
 
-const drawerWidth: number = 240
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}))
-
 const mdTheme = createTheme()
 
 export default function Layout({ children }: { children: React.ReactNode }) {
